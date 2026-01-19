dd8efefc748f563b5f6910580f69e4071fd6e586
PuneethThurimella <puneeth4800@gmail.com>
2026-01-19 16:12:31 +0530

    longest word string

---
+++ longest-word-string.js
@@ -0,0 +1,13 @@
+let string = "Puneeth Kuamr";
+
+function longestWord(str) {
+  let words = str.split(" ");
+  let longestWord = "";
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+console.log(longestWord(string));
