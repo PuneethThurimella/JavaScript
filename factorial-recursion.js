2c41a6d2e523fb5d3a308d511b3adbc7bd86da77
PuneethThurimella <puneeth4800@gmail.com>
2026-01-19 16:21:44 +0530

    factorial & recursion

---
+++ factorial-recursion.js
@@ -0,0 +1,18 @@
+// Recursion is a technique where a function calls itself to solve a problem.
+
+function factorial(n) {
+  if (n < 0) return "Invalid input";
+  if (n === 0 || n === 1) return 1; // recursion
+  return n * factorial(n - 1);
+}
+console.log(factorial(5));
+
+function Factorial(n) {
+  if (n < 0) return "Invalid input";
+  let fact = 1;
+  for (let i = 2; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
+}
+console.log(Factorial(5));
