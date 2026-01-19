07288393f82a2115b1824dc739a347d691cf0886
PuneethThurimella <puneeth4800@gmail.com>
2026-01-19 17:59:00 +0530

    smallest number array

--- smallest-number-array.js
+++ smallest-number-array.js
@@ -1,9 +1,52 @@
 const array = [18, 13, 12, 14, 16, 13, 15, 14];
 
-function smallestNumber(array, n) {
+function smallestNumber(arr) {
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+console.log(smallestNumber(array));
+
+function secondSmallest(arr) {
+  let first = Infinity;
+  let second = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] < second && arr[i] !== first) {
+      second = arr[i];
+    }
+  }
+  return second;
+}
+console.log(secondSmallest(array));
+
+function nthSmallest(array, n) {
   if (n > array.length) return "No elements found";
   let uniqueArray = [...new Set(array)].sort();
   console.log(uniqueArray);
   return uniqueArray[n - 1];
 }
-console.log(smallestNumber(array, 2));
+console.log(nthSmallest(array, 2));
+
+function nthSmallestNumber(arr, n) {
+  let prevMin = -Infinity;
+  let currentMin;
+  for (let count = 0; count < n; count++) {
+    currentMin = Infinity;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > prevMin && arr[i] < currentMin) {
+        currentMin = arr[i];
+      }
+    }
+    if (currentMin === Infinity) return "No element found";
+    prevMin = currentMin;
+  }
+  return currentMin;
+}
+console.log(nthSmallestNumber(array, 2));
