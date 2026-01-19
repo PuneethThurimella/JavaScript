ad1d49fe092939f9d2ee9272dbc5750be8bc727d
PuneethThurimella <puneeth4800@gmail.com>
2026-01-19 14:14:12 +0530

    reverseString &  Palindrome &  Anagrams

--- reverse-string.js
+++ reverse-string.js
@@ -3,7 +3,6 @@ let value = "Puneeth Kumar";
 function reverseString(string) {
   return string.split("").reverse().join("");
 }
-
 console.log(reverseString(value));
 
 function reverseStr(str) {
@@ -15,5 +14,12 @@ function reverseStr(str) {
   return result;
   // return result === str; // Palindrome
 }
-
 console.log(reverseStr(value));
+
+function areAnagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let sorted1 = str1.split("").sort().join("");
+  let sorted2 = str2.split("").sort().join("");
+  return sorted1 === sorted2;
+}
+console.log(areAnagrams("listen", "silent")); //true
