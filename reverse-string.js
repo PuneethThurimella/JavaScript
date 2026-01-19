cea0f7038faef6ad36556fcdfc0d78cbd712ce90
PuneethThurimella <puneeth4800@gmail.com>
2026-01-19 12:26:45 +0530

    palindrome & reverse string

--- reverse-string.js
+++ reverse-string.js
@@ -7,11 +7,13 @@ function reverseString(string) {
 console.log(reverseString(value));
 
 function reverseStr(str) {
+  // str = str.toLowerCase(); // Palindrome
   let result = "";
   for (let i = str.length - 1; i >= 0; i--) {
     result = result + str[i];
   }
   return result;
+  // return result === str; // Palindrome
 }
 
 console.log(reverseStr(value));
