6067310306dbe025f78878f6edc7996fb3590223
PuneethThurimella <puneeth4800@gmail.com>
2026-01-31 11:14:28 +0530

    javascript

--- Array Programs/find-duplicates-array.js
+++ Array Programs/find-duplicates-array.js
@@ -10,16 +10,4 @@ function findDuplicates(arr) {
   }
   return dup;
 }
-function findduplicates(arr) {
-  const seen = new Set();
-  const duplicates = new Set();
-  for (const item of arr) {
-    if (seen.has(item)) {
-      duplicates.add(item);
-    } else {
-      seen.add(item);
-    }
-  }
-  return [...duplicates];
-}
-console.log(findduplicates([18, 13, 12, 14, 16, 13, 15, 14, 16]));
+console.log(findDuplicates([18, 13, 12, 14, 16, 13, 15, 14, 16]));
