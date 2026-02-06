a1c0e89d1ce56a84e3401f541b8efe4793f80c4a
PuneethThurimella <puneeth4800@gmail.com>
2026-02-06 16:05:00 +0530

    javascript

--- Array Programs/sort-array.js
+++ Array Programs/sort-array.js
@@ -1,3 +1,14 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
@@ -8,6 +19,7 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+
 function isSorted(arr) {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < arr[i - 1]) return false;
