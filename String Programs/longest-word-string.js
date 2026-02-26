b841fe177fae2a0372528213b285ac3581ff9c48
PuneethThurimella <puneeth4800@gmail.com>
2026-02-26 10:37:28 +0530

    javascript

--- String Programs/longest-word-string.js
+++ String Programs/longest-word-string.js
@@ -12,19 +12,3 @@ function longestWord(str) {
 function LongestWord(str) {
   return str.split(" ").reduce((longest, word) => (word.length > longest.length ? word : longest),"");
 }
-
-function longestword(str) {
-  let longest = "";
-  let current = "";
-  for (let i = 0; i <= str.length; i++) {
-    if (str[i] !== " " && i !== str.length) {
-      current += str[i];
-    } else {
-      if (current.length > longest.length) {
-        longest = current;
-      }
-      current = "";
-    }
-  }
-  return longest;
-}
